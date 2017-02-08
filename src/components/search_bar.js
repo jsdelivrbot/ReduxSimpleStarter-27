8a3b7cadd625e1c5d75bdb7e2a9099285632110f
gerCasas <gcasasg4@gmail.com>
2017-02-08 14:08:54 -0800

    15 - Class-Based Components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+// { Component } es equivalente a poner:
+// const Component = React.Component
 
-const SearchBar = () => {
-  return <input />;
-};
+//se cambio por una clase en vez de una funcion por que ocupamos refenciar el input.
+class SearchBar extends Component {
+  //cada clase debe tener un render() function
+  render() {
+    return <input />;
+  }
+}
 
-// export sirve para que cualquier otro componente que implemente SearchBar obtenga ese componente. 
+// export sirve para que cualquier otro componente que implemente SearchBar obtenga ese componente.
 export default SearchBar;
