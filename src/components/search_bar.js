cd91f04fc169643bcec81d1cb2289082e7472f4f
gerCasas <gcasasg4@gmail.com>
2017-02-08 15:12:32 -0800

    18 - More on State
    Guardando el valor del input onChange en el State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,18 +4,22 @@ import React, { Component } from 'react';
 
 //se cambio por una clase en vez de una funcion por que ocupamos refenciar el input.
 class SearchBar extends Component {
-
   constructor(props) {
     super(props);
-
-    this.state = { term: '' }
+    // dentro de state podemos guardar nuestra variable, en este caso la llamamos 'term'
+    this.state = { term: '' };
   }
 
   //cada clase debe tener un render() function
   render() {
     // Se puede utilizar "arrow functions" como en la segunda linea o se puede mandar a llamar el metodo onInputChange, la primera opcion es la mas corta y con menos codigo ya que no tienes que declarar un metodo y luego mandarlo a llamar.
     // return <input onChange={this.onInputChange} />;
-    return <input onChange={(event) => console.log(event.target.value)} />;
+    // return <input onChange={(event) => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange = {(event) => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 
   //creacion de nuevo metodo para utilizarlo como handler del onChange del input
