41bea5823843cea1cac6906070d730a7b3ddf490
gerCasas <gcasasg4@gmail.com>
2017-02-08 13:57:51 -0800

    1st commit (Hasta 14 lecture)

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return <input />;
+};
+
+// export sirve para que cualquier otro componente que implemente SearchBar obtenga ese componente. 
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+//para importar react al archivo, el return <div>Hola!</div>; es JSX y en realidad regresa JS con referencia al modulo de react.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// como estamos importando un componente que nosostros creamos entonces tenemos que especificar toda la ruta se este componente. A diferencia de los imports de arriba, ya que los imports de arriba son package que instalamos con npm.
+import SearchBar from './components/search_bar';
+
+// api key para hacer request a youtube
+const API_KEY = 'AIzaSyAPBW29xL-BfL8lHca1yUspMwJBlXvHOxE';
+
+//crear un nuevo componente. produce HTML
+//se puso const en vez de var por que no se piensa cambiar el valors de App
+// const App = function(){ y const App = () => {  es lo mismo
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+//Take this component's generated HTML and put it on the page (in the DOM)
+//para mandar App como instancia en vez de una clase con encapsulamos con < />
+// '.container' se refiere a <div class="container"></div> en body del archivo index.html en la raiz del proyecto.
+ReactDOM.render(<App />, document.querySelector('.container'));
