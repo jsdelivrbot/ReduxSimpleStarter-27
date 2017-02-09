8013d9849e573f0a2851f393c8015efcac8571bc
gerCasas <gcasasg4@gmail.com>
2017-02-08 16:23:39 -0800

    22 - Refactoring Functional Components to Class Components
    Cambio de App de función a clase. YTSearch ejecutado en el constructor
    de App, el arreglo de respuesta guardado en videos [] .

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 //para importar react al archivo, el return <div>Hola!</div>; es JSX y en realidad regresa JS con referencia al modulo de react.
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 // como estamos importando un componente que nosostros creamos entonces tenemos que especificar toda la ruta se este componente. A diferencia de los imports de arriba, ya que los imports de arriba son package que instalamos con npm.
 import SearchBar from './components/search_bar';
-
 // api key para hacer request a youtube
 const API_KEY = 'AIzaSyAPBW29xL-BfL8lHca1yUspMwJBlXvHOxE';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+// YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+//   console.log(data);
+// });
 
 //crear un nuevo componente. produce HTML
 //se puso const en vez de var por que no se piensa cambiar el valors de App
 // const App = function(){ y const App = () => {  es lo mismo
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // this.setState({ videos: videos });
+      this.setState({ videos }); //si el key y value tienen el mismo nombre se puede dejar de esta forma.
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
