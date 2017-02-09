27b1cc8a0da15da0207198e6e559b3dc3eca60aa
gerCasas <gcasasg4@gmail.com>
2017-02-08 16:04:04 -0800

    21 - Youtube Search Response
    importación del modulo youtube search y búsqueda amarrada para ver que
    regresa de información.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 //para importar react al archivo, el return <div>Hola!</div>; es JSX y en realidad regresa JS con referencia al modulo de react.
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 // como estamos importando un componente que nosostros creamos entonces tenemos que especificar toda la ruta se este componente. A diferencia de los imports de arriba, ya que los imports de arriba son package que instalamos con npm.
 import SearchBar from './components/search_bar';
 
 // api key para hacer request a youtube
 const API_KEY = 'AIzaSyAPBW29xL-BfL8lHca1yUspMwJBlXvHOxE';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 //crear un nuevo componente. produce HTML
 //se puso const en vez de var por que no se piensa cambiar el valors de App
